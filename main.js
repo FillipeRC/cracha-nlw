b8115c4e0b3bc1187c2450791c74cb64810e00cc
unknown <fillipecoelho.fc@gmail.com>
2021-10-22 21:19:50 -0400

    first commit of cracha-nlw

---
+++ main.js
@@ -0,0 +1,37 @@
+const LinksSocialMedia = {
+  github: 'FillipeRC',
+  youtube: 'RocketSeat',
+  facebook: 'coelhofillipe',
+  instagram: 'fillipecoelho',
+  twitter: 'fillipecoelho1'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+
+  //template string
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url).then(response =>
+    response.json().then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      githubProfileLink.href = data.html_url
+      profilePhoto.src = data.avatar_url
+      userNickname.textContent = data.login
+    }
+    )
+  )
+}
+
+getGitHubProfileInfos()
